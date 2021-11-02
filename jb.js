cbe6bcc45fba50f2fd88de3413a8ba5e4094f54e
Chronoss <chronoss0109@gmail.com>
2021-11-02 02:00:08 +0100

    Add files via upload

--- jb.js
+++ jb.js
@@ -56847,3 +56847,15 @@ var printf_ans = read_mem_as_string(printf_buf, printf_buf_end-printf_buf);
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
+
+if (main_ret == 179 || main_ret == 0) {
+	localStorage.passcount = ++localStorage.passcount;window.passCounter.innerHTML=localStorage.passcount;
+	window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Exploit Loaded ✔ Now Loading HEN ...</h1>";
+    setTimeout(function(){
+    plLoader();
+    },1000);
+} 
+else {
+	localStorage.failcount = ++localStorage.failcount;window.failCounter.innerHTML=localStorage.failcount;
+	window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Jailbreak failed! - Reboot your PS4 and try again.</h1>";
+}
